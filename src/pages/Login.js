67fde47961c838fe41fcb1a69a8dbb83d9b52dba
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-09-19 19:55:32 +0530

    2 files modified

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,25 @@ export default function Login() {
   return (
     <div>
       <h1>Login </h1>
+      <form>
+        <div className="login">
+          <div className="form-control">
+            <label>Username:</label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div className="form-control">
+            <label>Password:</label>
+            <input type="text" name="password" id="password" />
+          </div>
+          <div>
+            <div className="form-control">
+              <label>Do you want to save your password:</label>
+              <input type="checkbox" name="username" id="username" />
+            </div>
+            <input type="submit" value="Login" />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,39 @@
 import React from 'react';
-export default function SignUp() {
+export default function Signup() {
   return (
     <div>
-      <h1>SignUp </h1>
+      <h1>Registration</h1>
+      <form>
+        <div className="signup">
+          <div className="form-control">
+            <label>Username:</label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div className="form-control">
+            <label>Password:</label>
+            <input type="text" name="password" id="password" />
+          </div>
+          <div className="form-control">
+            <label>Display Name:</label>
+            <input type="text" name="dname" id="dname" />
+          </div>
+          <div className="form-control">
+            <label>Email:</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="form-control">
+            <label>Upload photo:</label>
+            <input type="file" name="file" id="file" />
+          </div>
+          <div>
+            <div className="form-control">
+              <label>Agree Terms and conditions:</label>
+              <input type="checkbox" name="username" id="username" />
+            </div>
+            <input type="submit" value="Sign Up" />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
